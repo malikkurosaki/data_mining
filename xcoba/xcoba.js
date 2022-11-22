85f938d49d9096594baa7e19e3e71f0ca20c7632
mac <mac@macs-MacBook-Pro.local>
2022-11-22 23:58:17 +0800

    tambah coba

--- xcoba/xcoba.js
+++ xcoba/xcoba.js
@@ -1,45 +1,13 @@
-const prisma = new (require('@prisma/client').PrismaClient)()
-const moment = require('moment');
 const puppeteer = require('puppeteer');
-const { axios } = require('./importer');
-async function main() {
-    // let data = await prisma.youtubeContent.groupBy({
-    //     by: ["keywordId"],
-    //     _count: {
-    //         _all: true,
-    //     },
-    //     orderBy: {
-    //         _count: {
-    //             source: "desc"
-    //         }
-    //     },
-    //     where: {
-    //         createdAt: {
-    //             gte: new Date(moment("2021-11-06").format('YYYY-MM-DD')),
-    //         }
-    //     }
-    // })
 
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  // await page.screenshot({path: 'example.png'});
 
-    // console.log(data)
-
-    // let data = await prisma.youtubeContent.findMany({
-    //     where: {
-    //         Keyword: {
-    //             name: "Prabowo Subianto"
-    //         }
-    //     },
-    //     select: {
-    //         title: true
-    //     }
-    // })
-
-    // console.log(data.length)
-
-   const ax = await axios('http://localhost:3000/api/dashboard/dashboard-test-socket');
-   console.log(ax.data)
-
-   
- }
-
-main();
+  await browser.close();
+  console.log("ok docki")
+})();
