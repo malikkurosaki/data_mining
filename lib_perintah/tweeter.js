521cea0683d8ad141d6c98d383f4dab1c66be288
mac <mac@macs-MacBook-Pro.local>
2022-11-24 16:55:27 +0800

    add user agent

--- lib_perintah/tweeter.js
+++ lib_perintah/tweeter.js
@@ -28,14 +28,14 @@ async function main(keyword) {
         page = pg;
     }
 
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+
     console.log("membuka target")
     await page.goto(`https://twitter.com/search?q=${keyword.name
         }&src=typeahead_click&f=live`, {
         waitUntil: "networkidle2",
         timeout: 0
     });
-
-
 
     // gak dipake dulu karena gak mempan dapetnya segitue aja
     // await cobaScroll(page);
