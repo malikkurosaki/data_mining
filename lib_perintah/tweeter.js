612ad313c4e97d30356e8da7a74b6ed9b7bfeecf
mac <mac@macs-MacBook-Pro.local>
2022-11-24 17:00:56 +0800

    tambah cookies

--- lib_perintah/tweeter.js
+++ lib_perintah/tweeter.js
@@ -28,7 +28,7 @@ async function main(keyword) {
         page = pg;
     }
 
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+    // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
 
     console.log("membuka target")
     await page.goto(`https://twitter.com/search?q=${keyword.name
