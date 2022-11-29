9fd66a3e1a91b629782722317a5579ba3d4ca283
mac <mac@macs-MacBook-Pro.local>
2022-11-29 14:06:35 +0800

    edit terminal

--- xcoba.js
+++ xcoba.js
@@ -1,10 +1,31 @@
+const prisma = new (require('@prisma/client').PrismaClient)()
 
 var hitung = 0;
 async function main() {
 
-    setInterval(() => {
-        console.log("coba " + hitung++)
-    }, 1000)
+    const listKeyword = await prisma.keyword.findMany({
+        select: {
+            id: true,
+            name: true
+        }
+    })
+
+    for (let itm of listKeyword) {
+        itm['count'] = await prisma.googleNews.aggregate({
+            _count: {
+                _all: true
+            },
+            where: {
+                keywordId: {
+                    equals: itm.id
+                }
+            }
+        })
+
+        itm['count'] = itm._count._all
+    }
+
+    console.log(listKeyword)
 
 }
 
